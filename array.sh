61fd5e22d4403e940a14fe635b1cc186b43b46c6
rmCoder7 <mohammedayon2021@gmail.com>
2025-03-30 16:48:11 +0600

    add

---
+++ array.sh
@@ -0,0 +1,7 @@
+#Array is basically storing multipale data in singel variable
+
+
+myArray=(1 2 3.5 "Hello World")
+echo print ${myArray[3]}
+
+#here array value count from 0 start to end, like 5 variable will start 0 - 4.
---
+++ shell_01.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+## donot take any gape during varibale decailre
+
+
+name=Rahim
+Age=30
+gender=male
+
+echo My name is $name and my age is $age and also my gender is $gender.
+
+
+#second method
+
+name=rahul
+
+echo my name is $name
+
+
+
+## if we want to decalire my variable constant the we have addedd "readonly"
+
+readonly collegename= UNITAR
+
+
+echo my name is $name and my college name is $collegename
+
+
+
+
+
+
+
