ca073f3ed79e1f01ebc067afe17e5ab152fe242e
rmCoder7 <mohammedayon2021@gmail.com>
2025-03-31 14:44:34 +0600

    add

--- array.sh
+++ array.sh
@@ -5,3 +5,9 @@ myArray=(1 2 3.5 "Hello World")
 echo print ${myArray[3]}
 
 #here array value count from 0 start to end, like 5 variable will start 0 - 4.
+
+#show the uotput the array
+
+echo ${#myArray[*]}  #this use for print array's length like, * use for full array's length
+
+
