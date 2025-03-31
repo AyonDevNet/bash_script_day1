49a4529f0022ac96ae427a059c9ee90cdb94ac5c
rmCoder7 <mohammedayon2021@gmail.com>
2025-03-31 15:22:57 +0600

    add

--- array.sh
+++ array.sh
@@ -9,5 +9,9 @@ echo print ${myArray[3]}
 #show the uotput the array
 
 echo ${#myArray[*]}  #this use for print array's length like, * use for full array's length
+echo ${myArray[*]:1:2}  #Now if we want to find out a particular value’s length from the array.
+echo ${myArray[*]:2:2} # donot need to hash use hash(#) mark here
 
+myArray+=(10 20 30 40)
+echo ${#myArray[*]} #Just type it with plus marks that will indicate your array is updated.
 
