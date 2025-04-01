8381a1f744dc472267a57cb24bb0d4624deb7317
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-02 00:17:43 +0600

    add

---
+++ Arithmatic.sh
@@ -0,0 +1,19 @@
+a=3
+b=4
+let sum=a+b  # let is telling them that both are mathmatic equation not a string
+let sum1=a-b
+let sum2=a*b
+echo ${sum}
+echo ${sum1}
+echo ${sum2}
+
+
+# it will increase with 1 similar with - 
+
+c=10
+let c++
+echo ${c}
+
+r=3
+let r--
+echo ${r}
