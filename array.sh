905401a02fc38606abf73fcae07098ea5e7d25ee
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-01 12:26:41 +0600

    add

--- array.sh
+++ array.sh
@@ -12,6 +12,77 @@ echo ${#myArray[*]}  #this use for print array's length like, * use for full arr
 echo ${myArray[*]:1:2}  #Now if we want to find out a particular value’s length from the array.
 echo ${myArray[*]:2:2} # donot need to hash use hash(#) mark here
 
+#how to update Array's value
 myArray+=(10 20 30 40)
 echo ${#myArray[*]} #Just type it with plus marks that will indicate your array is updated.
 
+
+# Associative Arrays (Key-Value Pairs)
+#Associative arrays allow you to use strings as keys instead of just numbers. These require explicit declaration using declare -A.
+
+<<comment
+Meaning of -A
+declare -A array_name tells Bash that array_name should be an associative array, meaning it will use string keys instead of numerical indexes.
+comment
+
+#method-1
+
+declare -A myArray2
+myArray2=(
+    [name]=Rahim 
+    [age]=28 
+    [country]=Malayshia)
+
+echo My name is ${myArray2[name]}
+echo My age is ${myArray2[age]}
+echo I am from ${myArray2[country]}
+
+
+#method-2
+declare -A mydata 
+mydata=( 
+    [name1]=Rahim2 
+    [age1]=282 
+    [city1]=Malaysia2 )
+
+echo My name is ${mydata[name1]}
+echo My age is ${mydata[age1]}
+echo I am from ${mydata[city1]}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ tempCodeRunnerFile.sh
@@ -0,0 +1,3 @@
+declaire -A dataBase
+dataBase=([student1]=karim [roll1]=10 [school]=UNITAR)
+echo 10th student name is ${dataBase[student1]}
