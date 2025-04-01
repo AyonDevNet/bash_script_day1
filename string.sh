199cd155d4f3452e21469b49bf735f08cab1f286
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-01 16:23:36 +0600

    add

--- string.sh
+++ string.sh
@@ -1,3 +1,5 @@
+#To findout the lenth of Strings    
+
 myVar="Hello Buddy, How are you"
 myvarlength=${#myVar}
 echo my vhar length is ${myvarlength}
@@ -5,3 +7,7 @@ echo my vhar length is ${myvarlength}
 my_string="Hello, World!"
 length=${#my_string}
 echo "Length of string: $length"
+
+mystrinNew="Hello world , i am new here"
+storestring=${mystrinNew:19:26}
+echo the execution will be ${storestring}
