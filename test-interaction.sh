d14f98a3d40a266639c2c2611f11504f830d146d
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-01 22:14:38 +0600

    add

---
+++ test-interaction.sh
@@ -0,0 +1,14 @@
+read -p "please enter your name" name
+echo your name is ${name}
+
+read -p "Please enter you class id" ID
+echo your class ID is ${ID}
+
+
+read -p "Please enter you section" section
+echo your section is ${section}
+
+read -p "check it again. if it ok then type ok or No" answer
+echo your result is ${answer}
+
+
--- user_interaction.sh
+++ user_interaction.sh
@@ -1,2 +1,25 @@
+# when we add read it will automatic ask for your input
+#method -1 
+
 read name
-echo your name is $name
+#echo your name is $name
+
+
+#method-2
+#echo what is your name? -
+read age
+#echo your age is ${age}
+
+
+#method -3
+
+read -p "tell me about yourself" details
+echo thanks for your inforamation and check it again ${details}
+
+
+
+
+
+
+
+
