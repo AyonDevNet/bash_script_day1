4cfe1162a3fd013ae03609b3ba8d6f5a8c3b6f82
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-02 22:32:10 +0600

    add

---
+++ case.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Enter a color:"
+read color
+
+case $color in
+    red)
+        echo "You chose red!"
+       ;;
+    blue)
+        echo "You chose blue!"
+        ;;
+    green)
+        echo "You chose green!"
+      ;;
+    *)
+        echo "Invalid choice!"
+        
+esac
+
+read result
+
+case $result in
+   8[0-9]|9[0-9]|100)
+   echo "You got A+"
+   ;;
+
+   4[0-9]|5[0-9]|6[0-9]|7[0-9])  
+   echo "pass the exam"
+   ;;
+
+   [0-3][0-9]) 
+   echo "You fail"
+   ;;
+    
+   *)
+    echo "Invalid choice!"
+        
+esac
+
+read -p "Choose an option (start/stop/restart): " action
+
+case $action in
+    start) echo "Starting service..." ;;
+    stop) echo "Stopping service..." ;;
+    restart) echo "Restarting service..." ;;
+    *) echo "Invalid option!" ;;
+esac
