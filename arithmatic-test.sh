adcc12dfda49e7f5e4381d8aac92e30bc5efe4aa
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-02 11:31:21 +0600

    add

---
+++ arithmatic-test.sh
@@ -0,0 +1,6 @@
+#Write a script that takes two numbers as input and prints their sum.
+
+read -p "Enter the number :" a
+read -p "Enter the number :" b
+let sum=a+b
+echo ${sum}
---
+++ conditional.sh
@@ -0,0 +1,15 @@
+num=10
+if [ $num -gt 5 ]; then
+  echo "the number is bigger"
+
+fi
+
+
+num2=80
+num3=33
+
+if [ $num3 -ge 33 ]; then
+    echo " you pass the exam"
+elif [ $num2 -ge 80 ]; then
+     echo "You got A+"
+fi  
