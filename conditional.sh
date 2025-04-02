396690b2ff2ab243c22402628ea18f35cfda821e
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-02 11:43:19 +0600

    add

--- conditional.sh
+++ conditional.sh
@@ -5,6 +5,9 @@ if [ $num -gt 5 ]; then
 fi
 
 
+
+
+#method-2 if and elif condition
 num2=80
 num3=33
 
@@ -13,3 +16,20 @@ if [ $num3 -ge 33 ]; then
 elif [ $num2 -ge 80 ]; then
      echo "You got A+"
 fi  
+
+
+#method 3, taking input from user with conditions
+#double [[]] and singel [] both are same thinf , double for just advance
+read -p "Enter you marks here : " marks
+
+if [[ $marks -ge 40 ]]; then
+ echo "you pass the exam"
+
+ elif [[ $marks -ge 80 ]]; then
+ echo "You got A+ !!!!! "
+
+ elif [[ $marks -le 39 ]]; then 
+ echo "You got fail in the exam"
+ 
+ fi
+
