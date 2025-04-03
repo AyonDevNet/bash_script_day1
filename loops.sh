e5ab5f6dc1835b222bbc7f5ea4cae033f284dcc3
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-03 20:40:19 +0600

    add

--- loops.sh
+++ loops.sh
@@ -63,3 +63,16 @@ if (( n%2 == 0 )); then
 echo "the even number is $n"
 fi
 done
+
+
+
+while true
+do
+    read -p "Enter a number: " input
+    if [[ "$input" =~ ^[0-9]+$ ]]; then
+        echo "You entered a valid number: $input"
+        break
+    else
+        echo "Please enter a valid number."
+    fi
+done
--- while_loop.sh
+++ while_loop.sh
@@ -10,6 +10,19 @@ do
       echo "the number is $count"
       let count++
 done
-    
+
+
+
+while true
+do
+    read -p "Enter a number: " input
+    if [[ "$input" =~ ^[0-9]+$ ]]; then
+        echo "You entered a valid number: $input"
+        break
+    else
+        echo "Please enter a valid number."
+    fi
+done
+
 
   
