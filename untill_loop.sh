021230d5271d0a9637d1d0e17f534bbc94b5002e
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-05 17:40:57 +0600

    add

---
+++ untill_loop.sh
@@ -0,0 +1,5 @@
+<<< comment
+
+The until loop in shell scripting is kind of like the opposite of a while loop. It keeps running 
+until the condition becomes true (whereas while loops run while the condition is true).
+comment
