101733e083abe7fc15da29df4a0c68e5a1ae4209
rmCoder7 <mohammedayon2021@gmail.com>
2025-04-05 17:54:24 +0600

    add

--- untill_loop.sh
+++ untill_loop.sh
@@ -1,5 +1,27 @@
-<<< comment
 
-The until loop in shell scripting is kind of like the opposite of a while loop. It keeps running 
-until the condition becomes true (whereas while loops run while the condition is true).
-comment
+#comment
+#The until loop in shell scripting is kind of like the opposite of a while loop. It keeps running 
+#until the condition becomes true (whereas while loops run while the condition is true).
+#until [ condition ]
+#do
+    # commands
+#done
+#comment
+
+
+count=1
+
+until [ $count -gt 5 ]
+do
+    echo "Count is $count"
+    ((count++))
+done
+
+
+count2=10
+
+until [[ $count2 -eq 1 ]]
+do
+  echo "this is number" $count2
+  let $count2 --
+done
