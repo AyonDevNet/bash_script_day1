34d755fbf0692df75f426730e9e94d88c166963c
AyonDevNet <rmcoder7@gmail.com>
2025-04-16 16:07:34 +0600

    add

--- argument.sh
+++ argument.sh
@@ -1,1 +1,19 @@
-echo "all mighty push"
+<<< comment
+In a Bash script, an argument refers to a value that is passed to the script or to a function within the script when it is executed. These arguments can be used within the script to influence its behavior or perform specific tasks.
+
+Arguments are typically passed to the script when it is run from the command line, and they are accessed by their position in the command line using special variables.
+
+
+comment
+
+
+
+#to decalire first we need to add the echo and $, also a number
+#before decalire just said name , like , bash filename and what i wanna input here
+
+echo "Your name $1"
+echo "Your ID $2"
+echo "Your class section $3"
+echo "Your Gender $4"
+echo "Your Group $5"
+
