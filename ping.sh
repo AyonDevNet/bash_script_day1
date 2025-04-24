f2f06c6dc7e3ceb2dad5709c11417b5a5eb9c66b
AyonDevNet <rmcoder7@gmail.com>
2025-04-24 17:33:09 +0600

    add

--- ping.sh
+++ ping.sh
@@ -1,1 +1,13 @@
-#using of ping comman
+#using of ping command
+
+read -p "Enter you website : " site
+
+ping -c 1 $site
+
+if (( $? == 0 )); then
+
+  echo "This site is reachable : $site"
+
+else 
+    echo "This site is not reachable : $site"
+fi
