b4a041b549269c6c94bf2e04c29a36b825003fd9
AyonDevNet <rmcoder7@gmail.com>
2025-04-27 15:57:03 +0600

    add

--- ping.sh
+++ ping.sh
@@ -13,12 +13,5 @@ else
 fi
 
 
-read -p "Enter the websot nname : " site
-
-ping -c 1 $site > /dev/null 2>&1
-
-if (( $? == 0 )); then
-    echo "This site is reachable " $site
-then
-    echo "this site is not reachable " $site
-fi
+NO=$(( $RANDOM%6 + 1 ))
+echo $NO
---
+++ randum_NUM.sh
@@ -0,0 +1,4 @@
+### *** $RANDOM is basically generated the code 0 - 3267, is generated randomly.
+
+N0=$(( $RANDOM%6+1 ))
+echo "the number is " $N0
